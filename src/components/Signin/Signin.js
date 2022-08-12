b0118c24585737b13cfd709273bb1ee2f9747276
letsbehuman <letsbehumandesign@gmail.com>
2022-08-12 12:20:56 +0200

    url change

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,8 +18,8 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    console.log(this.state);
-    fetch('http://localhost:3000/signin', {
+    // console.log(this.state);
+    fetch('https://agile-citadel-98749.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
